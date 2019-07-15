26206090d00813a8a9ae25a6fdda9035e7373dc4
YOYO-Kira <1164020843@qq.com>
2019-07-15 14:46:08 +0800

    vue-shequ pro done

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue"
 
+import App from "./App.vue"
+import "./assets/glocal.css"
+import router from "./router"
+import "./plugins/element"
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router
+}).$mount("#app")
---
+++ src/plugins/element.js
@@ -0,0 +1,5 @@
+
+import Vue from "vue"
+
+import { Pagination } from "element-ui"
+Vue.use(Pagination)
